3321b203654beb4b29094630957370bd1c80f118
sebaherrera74 <sebaherrera152@gmail.com>
2021-12-16 21:35:57 -0300

    Agrego ejerccio de cronometro

--- projects/cronometro/src/blinking.c
+++ projects/cronometro/src/blinking.c
@@ -55,18 +55,7 @@
 #include "task.h"
 #include "soc.h"
 #include "led.h"
-#include  "semphr.h"
-#include "switch.h"
-#include "event_groups.h"
 /* === Definicion y Macros ================================================= */
-#define EVENTO_TECLA_1_ON 	( 1 << 0 )
-#define EVENTO_TECLA_2_ON 	( 1 << 1 )
-#define EVENTO_TECLA_3_ON 	( 1 << 2 )
-#define EVENTO_TECLA_4_ON 	( 1 << 3 )
-#define EVENTO_TECLA_1_OFF ( 1 << 4 )
-#define EVENTO_TECLA_2_OFF ( 1 << 5 )
-#define EVENTO_TECLA_3_OFF ( 1 << 6 )
-#define EVENTO_TECLA_4_OFF ( 1 << 7 )
 
 /* === Declaraciones de tipos de datos internos ============================ */
 /*Estructura para pasar por parametros a la tarea de prende y apaga led*/
@@ -86,12 +75,8 @@ typedef struct {
  **                           y la demora entre encendido y apagado.
  */ 
 void Blinking(void * parametros);
-void Teclas(void * parametros);
 
 /* === Definiciones de variables internas ================================== */
-
-SemaphoreHandle_t mutex;
-EventGroupHandle_t eventos;
 
 /* === Definiciones de variables externas ================================== */
 
@@ -99,100 +84,48 @@ EventGroupHandle_t eventos;
 
 void Blinking(void * parametros) {
 	blinking_t * valores = parametros;
-	EventBits_t uxBits;
-	while(1) {
 
-		uxBits=xEventGroupWaitBits(eventos, EVENTO_TECLA_1_ON, pdTRUE, pdFALSE, portMAX_DELAY);
-		xSemaphoreTake(mutex,portMAX_DELAY);
-		//Led_Toggle(valores->led);
-		Led_On(valores->led);
+	while(1) {
+		Led_Toggle(valores->led);
 		vTaskDelay(valores->delay/ portTICK_PERIOD_MS);
-		Led_Off(valores->led);
-		xSemaphoreGive(mutex);
-		vTaskDelay(valores->delay/ portTICK_PERIOD_MS);
-		//vTaskDelay(1/ portTICK_PERIOD_MS);
 	}
 }
+/* === Definiciones de funciones externas ================================== */
+
+/** @brief Función principal del programa
+ **
+ ** @returns 0 La función nunca debería termina
+ **
+ ** @remarks En un sistema embebido la función main() nunca debe terminar.
+ **          El valor de retorno 0 es para evitar un error en el compilador.
+ */
+int main(void) {
+
+	/* Variable con los parametros de las tareas */
+	static blinking_t valores[]={
+			{.led =RGB_B_LED, .delay = 500},
+			{.led =RGB_R_LED, .delay =300}
+	};
 
 
-void Teclas(void * parametros){
-	uint8_t anterior = 0;
-	uint8_t actual;
-	uint8_t cambios;
-	while(1){
-		actual = Read_Switches();
-		cambios = actual ^ anterior;
-
-		if (cambios & TECLA1){
-			if (actual & TECLA1){
-				xEventGroupSetBits(eventos, EVENTO_TECLA_1_ON);
-			} else {
-				xEventGroupSetBits(eventos, EVENTO_TECLA_1_OFF);
-			}
-		}
-		if (cambios & TECLA2){
-			if (actual & TECLA2){
-				xEventGroupSetBits(eventos, EVENTO_TECLA_2_ON);
-			} else {
-				xEventGroupSetBits(eventos, EVENTO_TECLA_2_OFF);
-			}
-		}
-		if (cambios & TECLA3){
-			if (actual & TECLA3){
-				xEventGroupSetBits(eventos, EVENTO_TECLA_3_ON);
-			} else {
-				xEventGroupSetBits(eventos, EVENTO_TECLA_3_OFF);
-			}
-		}
-		if (cambios & TECLA4){
-			if (actual & TECLA4){
-				xEventGroupSetBits(eventos, EVENTO_TECLA_4_ON);
-			} else {
-				xEventGroupSetBits(eventos, EVENTO_TECLA_4_OFF);
-			}
-		}
-		anterior = actual;
-		vTaskDelay(pdMS_TO_TICKS(150));
-	}}
-
-	/* === Definiciones de funciones externas ================================== */
-
-	/** @brief Función principal del programa
-	 **
-	 ** @returns 0 La función nunca debería termina
-	 **
-	 ** @remarks En un sistema embebido la función main() nunca debe terminar.
-	 **          El valor de retorno 0 es para evitar un error en el compilador.
-	 */
-	int main(void) {
-
-		/* Variable con los parametros de las tareas */
-		static blinking_t valores[]={
-				{.led =RGB_B_LED, .delay = 500},
-				{.led =RGB_R_LED, .delay =300}
-		};
 
 
-		mutex=xSemaphoreCreateMutex();
-        eventos=xEventGroupCreate();
+	/* Inicializaciones y configuraciones de dispositivos */
 
-		/* Inicializaciones y configuraciones de dispositivos */
+	SisTick_Init();
+	Init_Leds();
 
-		SisTick_Init();
-		Init_Leds();
+	/* Creación de las tareas */
+	xTaskCreate(Blinking, "Azul", configMINIMAL_STACK_SIZE, &valores[0], tskIDLE_PRIORITY + 1, NULL);
+	xTaskCreate(Blinking, "Rojo", configMINIMAL_STACK_SIZE, &valores[1], tskIDLE_PRIORITY + 1, NULL);
+	/* Arranque del sistema operativo */
+	vTaskStartScheduler();
 
-		/* Creación de las tareas */
-		xTaskCreate(Blinking, "Azul", configMINIMAL_STACK_SIZE, &valores[0], tskIDLE_PRIORITY + 1, NULL);
-		//xTaskCreate(Blinking, "Rojo", configMINIMAL_STACK_SIZE, &valores[1], tskIDLE_PRIORITY + 1, NULL);
-		xTaskCreate(Teclas, "Teclas", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-		/* Arranque del sistema operativo */
-		vTaskStartScheduler();
+	/* vTaskStartScheduler solo retorna si se detiene el sistema operativo */
+	while(1);
 
-		/* vTaskStartScheduler solo retorna si se detiene el sistema operativo */
-		while(1);
-
-		/* El valor de retorno es solo para evitar errores en el compilador*/
-		return 0;
-	}
-	/* === Ciere de documentacion ============================================== */
-	/** @} Final de la definición del modulo para doxygen */
+	/* El valor de retorno es solo para evitar errores en el compilador*/
+	return 0;
+}
+/* === Ciere de documentacion ============================================== */
+/** @} Final de la definición del modulo para doxygen */
